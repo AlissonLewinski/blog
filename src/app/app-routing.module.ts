aef33fb65ebb798f873bb2d285b0c0c6e385fc0e
Lucas <lucasali7@gmail.com>
2021-09-25 01:07:37 -0300

    init create post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePostComponent } from './content/create-post/create-post.component';
 import { PostsComponent } from './content/posts/posts.component';
 
 const routes: Routes = [
   {
     path: 'posts',
     component: PostsComponent
+  },
+  {
+    path: 'new',
+    component: CreatePostComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
-import { PostComponent } from './content/post/post.component';
+import { PostComponent } from './content/posts/post/post.component';
 import { PostsComponent } from './content/posts/posts.component';
+import { CreatePostComponent } from './content/create-post/create-post.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PostsComponent } from './content/posts/posts.component';
     HeaderComponent,
     ContentComponent,
     PostComponent,
-    PostsComponent
+    PostsComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
