b6b4ea4a0d00427db47792512ce81d9d31b32e5f
Lucas <lucasali7@gmail.com>
2021-09-26 03:05:19 -0300

    showing posts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from './posts.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts: Post[] = []
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.postsService.getAll().subscribe((res: any) => {
+      this.posts = res.posts
+    })
   }
 
 }
