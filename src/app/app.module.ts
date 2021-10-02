6d477ffa712ca3b20ea3095a9c15af92220f8ab8
Lucas <lucasali7@gmail.com>
2021-10-02 03:00:28 -0300

    leave quill behind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
-import { QuillModule } from 'ngx-quill';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NavComponent } from './header/nav/nav.component';
